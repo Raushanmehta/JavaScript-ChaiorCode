32061806c910a519e3f1f59bdb42b0898a410131
Raushan Mehta <142925155+Raushanmehta@users.noreply.github.com>
2024-05-04 20:13:44 +0000

    conversation of data types

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "raushan"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "25" = 25
+// "36abc" = NaN 
+// true = 1 
+// false = 0 
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// ""= false 
+// "raushan" = true 
+
+let isNumber = 654
+let stringIsNumber = String(isNumber)
+console.log(isNumber);
+console.log(typeof stringIsNumber)
