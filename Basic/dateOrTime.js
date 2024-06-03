62af55a51700031b8966be427b4fe31d64d2fb40
Raushan Mehta <142925155+Raushanmehta@users.noreply.github.com>
2024-06-03 09:03:44 +0000

    last seen 13 8:08

---
+++ Basic/dateOrTime.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+let myNewDate = new Date(2022,5,3)
+console.log(myNewDate.toDateString());
