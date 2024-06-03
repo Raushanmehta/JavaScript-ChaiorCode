2b4ae0231e6c51c411de18b80db5c63a794bf3c8
Raushan Mehta <142925155+Raushanmehta@users.noreply.github.com>
2024-06-03 00:26:48 +0000

    last seen 12V 11:32 maths

---
+++ Basic/numOrMath.js
@@ -0,0 +1,14 @@
+const score =  500
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const newNumber = 15455.8252
+console.log(newNumber.toPrecision(5))
+
+const bankBlance = 2006545465
+console.log(bankBlance.toLocaleString(`en-IN`))
