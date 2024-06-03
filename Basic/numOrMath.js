8557b44376f37d302c5bb118ea6063b902e05bf2
Raushan Mehta <142925155+Raushanmehta@users.noreply.github.com>
2024-06-03 08:49:58 +0000

    last seen 12 done

--- Basic/numOrMath.js
+++ Basic/numOrMath.js
@@ -1,14 +1,31 @@
-const score =  500
-console.log(score);
+const score = 500
+// console.log(score);
 
 const balance = new Number(200)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
 const newNumber = 15455.8252
-console.log(newNumber.toPrecision(5))
+// console.log(newNumber.toPrecision(5))
 
 const bankBlance = 2006545465
-console.log(bankBlance.toLocaleString(`en-IN`))
+// console.log(bankBlance.toLocaleString(`en-IN`))
+
+
+//**************************Maths**********************/
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(5.921));
+// console.log(Math.min(4,6,9,2,5,8));
+// console.log(Math.max(4,9,7,26,4,));
+
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 50
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
